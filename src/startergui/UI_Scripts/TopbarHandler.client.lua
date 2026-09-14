a099f786b445286e208fab27663333d0affb5ef3
Kr6d <kr6dii@gmail.com>
2026-09-13 22:38:06 -0500

    Implement TopbarHandler

--- src/startergui/UI_Scripts/TopbarHandler.client.lua
+++ src/startergui/UI_Scripts/TopbarHandler.client.lua
@@ -5,7 +5,6 @@ local RS = game:GetService("ReplicatedStorage")
 local Modules = RS:WaitForChild("Modules")
 local Icon = require(RS:WaitForChild("Icon"))
 local Utilities = require(Modules.Utilities)
-local spr = Utilities.spr
 
 --// Player & Loading
 local player = game.Players.LocalPlayer
@@ -14,27 +13,17 @@ repeat wait() until player:FindFirstChild("Loaded") and player.Loaded.Value or p
 if player.Parent == nil then return end
 
 --// Variables
-local blur = game:GetService("Lighting"):WaitForChild("LCBlurEffect")
-local fov = game:GetService("Workspace"):WaitForChild("Camera")
 local UI = player.PlayerGui.UI
-local buttonsFrame = UI.Canvas.Buttons
-local UIFrames = UI.Canvas.Frames
-local buttons = {}
-local frames = {}
-
---// Constants
-local DEBOUNCE = .35
-local BLUR_SHOWN = 15
-local BLUR_HIDDEN = 0
-local FOV_NORMAL = 70
-local FOV_ADJUSTED = 60
 
 --// Main
-local settings = Icon.new()
-    :setName("Settings")
-    :setImage("rbxassetid://11713339600", "Deselected")
-    :setImage("rbxassetid://134515081025675", "Selected")
-    :bindEvent("deselected", function()
-	    Utilities.UI.DisplayFrame(UIFrames.Settings, script.Settings)
-    end)
-    :oneClick()
+for _, config in script:GetChildren() do
+    local icon = Icon.new()
+        :setName(config.Name)
+        :bindEvent("deselected", function()
+            Utilities.UI.DisplayFrame(UI.Canvas.Frames:FindFirstChild(config.Name), config)
+        end)
+        :oneClick()
+    if config:GetAttribute("ImageID") then
+        icon:setImage(config:GetAttribute("ImageID"))
+    end
+end
